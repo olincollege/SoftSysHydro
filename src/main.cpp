65c12411de084fbf1e2f885a5c3c2f0984b9d5cf
lila <lsmith2@olin.edu>
2021-04-02 18:19:41 -0400

    Fixed MEGA configuration and added Atlas Scientific pH libs.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,20 @@
 #include <Arduino.h>
 #include <PhSensor.h>
 #include <PhUpPump.h>
+#include <ph_grav.h>
+
+Gravity_pH pH = A0; //Assigns pin for pH signal
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  PhSensor phSensor;
-  phSensor.ph = 7;
-  phSensor.sendSensorLog();
+  //PhSensor phSensor;
+  //phSensor.ph = 7;
+  //phSensor.sendSensorLog();
+  Serial.print("pH: ");
+  Serial.println(pH.read_ph());
   delay(3000);
   // put your main code here, to run repeatedly:
 }
